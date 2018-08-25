c8c6c97deb3dcf5e8d257355d855a6db386dfff4
ZYSzys <zyszys98@gmail.com>
2018-08-25 14:02:25 +0800

    specific language (#4)
    * Use commander
    
    * specific language
    
    * more test
    
    * Up coverage

--- test.js
+++ test.js
@@ -5,6 +5,30 @@ test.cb('sort torvalds starred repos', t => {
   sortGhStarredSize('torvalds', t.end)
 })
 
-test.cb('sort ZYSzys starred repos', t => {
-  sortGhStarredSize('ZYSzys', t.end)
+test.cb('sort ZYSzys starred js repos', t => {
+  sortGhStarredSize('ZYSzys', 'js', t.end)
+})
+
+test.cb('sort ZYSzys starred py repos', t => {
+  sortGhStarredSize('ZYSzys', 'py', t.end)
+})
+
+test.cb('sort ZYSzys starred ts repos', t => {
+  sortGhStarredSize('ZYSzys', 'ts', t.end)
+})
+
+test.cb('sort ZYSzys starred sh repos', t => {
+  sortGhStarredSize('ZYSzys', 'sh', t.end)
+})
+
+test.cb('sort ZYSzys starred md repos', t => {
+  sortGhStarredSize('ZYSzys', 'md', t.end)
+})
+
+test.cb('sort ZYSzys starred c repos', t => {
+  sortGhStarredSize('ZYSzys', 'c', t.end)
+})
+
+test.cb('sort ZYSzys starred c++ repos', t => {
+  sortGhStarredSize('ZYSzys', 'c++', t.end)
 })
