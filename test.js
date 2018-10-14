7de309269381f9a88478b6a12bd4799c985c6180
ZYSzys <17367077526@163.com>
2018-10-14 11:21:24 +0800

    refactor

--- test.js
+++ test.js
@@ -1,34 +1,34 @@
-import test from 'ava'
-import sortGhStarredSize from '.'
+import test from 'ava';
+import sortGhStarredSize from '.';
 
 test.cb('sort torvalds starred repos', t => {
-  sortGhStarredSize('torvalds', t.end)
-})
+  sortGhStarredSize('torvalds', t.end);
+});
 
 test.cb('sort ZYSzys starred js repos', t => {
-  sortGhStarredSize('ZYSzys', 'js', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'js', t.end);
+});
 
 test.cb('sort ZYSzys starred py repos', t => {
-  sortGhStarredSize('ZYSzys', 'py', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'py', t.end);
+});
 
 test.cb('sort ZYSzys starred ts repos', t => {
-  sortGhStarredSize('ZYSzys', 'ts', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'ts', t.end);
+});
 
 test.cb('sort ZYSzys starred sh repos', t => {
-  sortGhStarredSize('ZYSzys', 'sh', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'sh', t.end);
+});
 
 test.cb('sort ZYSzys starred md repos', t => {
-  sortGhStarredSize('ZYSzys', 'md', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'md', t.end);
+});
 
 test.cb('sort ZYSzys starred c repos', t => {
-  sortGhStarredSize('ZYSzys', 'c', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'c', t.end);
+});
 
 test.cb('sort ZYSzys starred c++ repos', t => {
-  sortGhStarredSize('ZYSzys', 'c++', t.end)
-})
+  sortGhStarredSize('ZYSzys', 'c++', t.end);
+});
